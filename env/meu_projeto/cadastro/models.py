efedc51aa2af8ee1436c2e8a1ea23bf45fda9fd4
Walisson oliveira <walissonoliveira1@outlook.com>
2024-12-02 22:27:14 -0300

    Alterações no tamanho dos campos da tabela

--- env/meu_projeto/cadastro/models.py
+++ env/meu_projeto/cadastro/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.hashers import make_password
 class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     data_nascimento = models.DateField()
-    cpf = models.CharField(max_length=12)
+    cpf = models.CharField(max_length=14)
     email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=128)  # Aumente o tamanho para acomodar o hash
-    cep = models.CharField(max_length=12)
-    uf = models.CharField(max_length=20)
-    bairro = models.CharField(max_length=20)
+    password = models.CharField(max_length=138)  # Aumente o tamanho para acomodar o hash
+    cep = models.CharField(max_length=15)
+    uf = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
     rua = models.CharField(max_length=100)
     numero = models.CharField(max_length=10)
     complemento = models.CharField(max_length=100, blank=True, null=True)
